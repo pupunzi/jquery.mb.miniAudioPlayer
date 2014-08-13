b6bb6c6ebaa3457e1120f5283901c4321369a62f
Matteo Bicocchi <matteo@open-lab.com>
2014-08-13 11:35:13 +0200

    modifying the ID3 behavior

--- inc/id3/id3.js
+++ inc/id3/id3.js
@@ -1,23 +1,3 @@
-/*
- * ******************************************************************************
- *  jquery.mb.components
- *  file: id3.js
- *
- *  Copyright (c) 2001-2014. Matteo Bicocchi (Pupunzi);
- *  Open lab srl, Firenze - Italy
- *  email: matteo@open-lab.com
- *  site: 	http://pupunzi.com
- *  blog:	http://pupunzi.open-lab.com
- * 	http://open-lab.com
- *
- *  Licences: MIT, GPL
- *  http://www.opensource.org/licenses/mit-license.php
- *  http://www.gnu.org/licenses/gpl.html
- *
- *  last modified: 07/01/14 22.50
- *  *****************************************************************************
- */
-
 var ID3 = {};
 
 (function() {
