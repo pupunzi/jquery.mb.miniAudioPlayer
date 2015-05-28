635f3bc2956c04173de4710b90afe9c4d69bc63c
mb.jquery.components <matteo@open-lab.com>
2015-05-28 21:54:12 +0200

    Release v1.8.6

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,7 +123,7 @@ module.exports = function(grunt) {
 				tagName: 'v%VERSION%',
 				tagMessage: 'Version %VERSION%',
 				push: true,
-				pushTo: 'master',
+				pushTo: 'push.default',
 				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
 				globalReplace: false,
 				prereleaseName: false,
